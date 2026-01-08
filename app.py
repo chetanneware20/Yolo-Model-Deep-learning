e3014712d298561a04ebcb4bf4b3a90060b515b9
chetan neware <newarechetan539@gmail.com>
2026-01-08 11:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from ultralytics import YOLO
 from PIL import Image
 import tempfile
 
-st.set_page_config(page_title="YOLO Detection", layout="centered")
+st.set_page_config(page_title="YOLO Detection")
 
 st.title("🧠 YOLO Object Detection")
 
 model = YOLO("yolov8n.pt")
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
-if uploaded_file:
-    image = Image.open(uploaded_file)
+if file:
+    image = Image.open(file)
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
@@ -20,5 +20,4 @@ if uploaded_file:
         results = model(tmp.name)
 
     st.subheader("Detection Result")
-    result_img = results[0].plot()
-    st.image(result_img, use_container_width=True)
+    st.image(results[0].plot(), use_container_width=True)
